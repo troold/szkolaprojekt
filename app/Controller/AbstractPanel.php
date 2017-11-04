40a353de8395a41c875a631f5a92fd6db1ebf077
troold <troold@gmail.com>
2017-11-04 11:18:09 +0100

    authlogin

--- app/Controller/AbstractPanel.php
+++ app/Controller/AbstractPanel.php
@@ -8,14 +8,10 @@ abstract class AbstractPanelController extends Controller {
      * Dynamiczny loader stron wykrywa akcje jako plik i stara sie go za ładować
      */
     public function start(){
-        $msg = new Messages(new Session('hashSaltRandomForSession')); // Join the current session
-        $msg->add('s', 'Success Message!');
-        //$msg->add('s', 'Success Message!', 'page/index'); // with redirect 
-        $msg->hasMessages('success'): // Will return array['success']
-        $msg->hasMessages(): // Will return all array
-
-        $msg->clear('success'); // remove success msg
-        $msg->clear(); // remove all msg
+        if(authLogin() != true)
+            return $this->baseClass->msg('e', 'Nie jestes zalogowany');
+        
+        $this->baseClass->msg('s', 'Success Message!');
     }
 
 }

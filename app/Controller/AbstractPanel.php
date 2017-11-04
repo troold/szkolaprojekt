b608010db513f3a2c57050a6e8e895a33cb417f8
troold <troold@gmail.com>
2017-11-04 11:27:15 +0100

    zmiana nazwy

--- app/Controller/AbstractPanel.php
+++ app/Controller/AbstractPanel.php
@@ -8,10 +8,11 @@ abstract class AbstractPanelController extends Controller {
      * Dynamiczny loader stron wykrywa akcje jako plik i stara sie go za ładować
      */
     public function start(){
+
         if(authLogin() != true)
-            return $this->baseClass->msg('e', 'Nie jestes zalogowany');
-        
-        $this->baseClass->msg('s', 'Success Message!');
+            return $this->baseClass->msg('e', 'Nie jestes zalogowany','{$router->publicWeb()');
+
+        $this->baseClass->msg('s', 'Success Message!','{$router->publicWeb()');
     }
 
 }
